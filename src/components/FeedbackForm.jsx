404f625eae27a6b8fe434c41b52c487894af3ad9
Reese <arter.charise@gmail.com>
2023-08-15 16:47:12 -0700

    added Button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,31 @@
-import PropTypes from 'prop-types'
-import Card from './shared/Card'
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import Card from './shared/Card';
+import Button from './shared/Button';
 
 function FeedbackForm() {
-  return (
-    <Card>
-      FeedbackForm
-    </Card>
-  )
+	const [text, setText] = useState('');
+
+	const handleTextChange = (e) => {
+		setText(e.target.value);
+	};
+	return (
+		<Card>
+			<form>
+				<h2>How would you rate this page? </h2>
+				{/* TODO: Rating slect component */}
+				<div className='input-group'>
+					<input
+						onChange={handleTextChange}
+						type='text'
+						placeholder='Write a review'
+						value={text}
+					/>
+					{/* Change button to own customizable component later */}
+					<Button type='submit'>Send</Button>
+				</div>
+			</form>
+		</Card>
+	);
 }
-export default FeedbackForm
+export default FeedbackForm;
