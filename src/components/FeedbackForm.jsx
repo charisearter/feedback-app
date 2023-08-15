39a3e62a3d72c3866747cbbc93ce20ac15508395
Reese <arter.charise@gmail.com>
2023-08-15 16:06:41 -0700

    building FeedbackForm

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+
+function FeedbackForm() {
+  return (
+    <Card>
+      FeedbackForm
+    </Card>
+  )
+}
+export default FeedbackForm
